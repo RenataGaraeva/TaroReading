7868988312759162ce8d46530fea76bd791d8522
RenataGaraeva <renatagaraeva12@gmail.com>
2026-02-07 02:17:03 +0300

    add page career

---
+++ app/[locale]/career/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import FortuneTelling from '@/app/Components/FortuneTelling/FortuneTelling'
+
+const CareerPage = () => {
+    return <FortuneTelling theme="career" />
+}
+
+export default CareerPage
